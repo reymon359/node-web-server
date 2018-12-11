7003ce26ae6f77267cbb5585f0aca7a5fbe04a62
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-11 17:41:34 +0100

    Added hbs partials to optimize views

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const app = express();
 
+const hbs = require('hbs');
+
 //middleware for the static public content.
 app.use(express.static(__dirname + '/public'));
 
 //Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
 
     res.render('home', {
         name: 'Ramon',
+        year: new Date().getFullYear()
+    });
+})
+
+app.get('/about', (req, res) => {
+
+    res.render('about', {
         year: new Date().getFullYear()
     });
 })
