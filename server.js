13bdc01730e49fe31e68d52198150448dd60cb01
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-11 17:02:57 +0100

    installed handlebars for express. New views folder for the html views

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express')
-const app = express()
+const app = express();
 
 //middleware for the static public content.
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', (req, res) => {
-//     // res.send('Hello World')
-//     let data = {
-//         name: 'ramon',
-//         age: 24,
-//         url: req.url
-//     }
-//     res.send(data);
-// })
+//Express HBS engine
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        name: 'Ramon',
+        year: new Date().getFullYear()
+    });
+})
 
 app.listen(3000, () => {
     console.log('Listening requests on port 3000');
