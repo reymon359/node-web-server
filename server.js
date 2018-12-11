4c83ed45564977e3bc17d80d5baaccf25c43aa8a
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-11 18:07:08 +0100

    Creating helpers for date and capitalize text.

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ const express = require('express')
 const app = express();
 
 const hbs = require('hbs');
+require('./hbs/helpers');
 
 //middleware for the static public content.
 app.use(express.static(__dirname + '/public'));
 
 //Express HBS engine
-hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
 
     res.render('home', {
-        name: 'Ramon',
+        name: 'raMon morciLlo',
         year: new Date().getFullYear()
     });
 })
-
 app.get('/about', (req, res) => {
 
     res.render('about', {
