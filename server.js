cbdae0591bb498b440de1fad1861a2bdc68affb1
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-11 09:52:28 +0100

    created a folder for public static content. Added a middleware for it

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    // res.send('Hello World')
-    let data = {
-        name: 'ramon',
-        age: 24,
-        url: req.url
-    }
-    res.send(data);
-})
+//middleware for the static public content.
+app.use(express.static(__dirname + '/public'));
 
+// app.get('/', (req, res) => {
+//     // res.send('Hello World')
+//     let data = {
+//         name: 'ramon',
+//         age: 24,
+//         url: req.url
+//     }
+//     res.send(data);
+// })
 
 app.listen(3000, () => {
     console.log('Listening requests on port 3000');
